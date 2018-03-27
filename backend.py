6bd0b19334725953e33104cdd4a388bb684031a6
LT11222 <luke11222@gmail.com>
2018-03-27 13:08:42 -0400

    Fixed data order, added average match time to features, moved model to own file

--- backend.py
+++ backend.py
@@ -6,19 +6,6 @@ import numpy
 from scipy import sparse
 import pandas
 
-import tensorflow as tf
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.Session(config=config)
-
-import keras
-from keras.models import Model
-from keras.layers import Input, Dense, Activation, Dropout, GaussianNoise
-from keras.optimizers import SGD, RMSprop, Adagrad, Adadelta, Adam, Adamax, Nadam
-from keras.regularizers import l2
-from keras.layers.normalization import BatchNormalization
-from keras.utils import plot_model
-
 def buildDict(data):
     data = [x[:2] for x in data]
     names = [x[0] for x in data]
@@ -45,6 +32,15 @@ def buildMatrix(data, nameDict, isSparse=1):
         name1, name2, winner, *rest = value
         nameMat[row,nameDict[name1]] = 1
         nameMat[row,nameDict[name2]] = 1
+
+        redStats = rest[:int(len(rest)/2)]
+        blueStats = rest[int(len(rest)/2):]
+
+        if(nameDict[name1] > nameDict[name2]):
+            rest = redStats+blueStats
+        elif(nameDict[name2] > nameDict[name1]):
+            rest = blueStats+redStats
+
         dataMat[row] = rest
         if winner == name1:
             labelMat[row,nameDict[name1]] = 1
@@ -62,7 +58,7 @@ def predict(model, data, labels, cutoff = 0.5):
     count = 0
     total = 0
 
-    cutoff = 0.5
+    cutoff = 0.0
 
     for i in range(data.shape[0]):
         dataVals = numpy.where(data[i])[0]
@@ -95,22 +91,6 @@ def dataGenerator(data, labels, batch_size=1):
             if i != 0 and i % batch_size == 0:
                 yield [[data[0][i-batch_size:i].todense(), data[1][i-batch_size:i]], [labels[0][i-batch_size:i].todense()]]
 
-def optimizer(name, val):
-    if name == "SGD":
-        return SGD(lr=val)
-    if name == "RMSprop":
-        return RMSprop(lr=val)
-    if name == "Adagrad":
-        return Adagrad(lr=val)
-    if name == "Adadelta":
-        return Adadelta(lr=val)
-    if name == "Adam":
-        return Adam(lr=val)
-    if name == "Adamax":
-        return Adamax(lr=val)
-    if name == "Nadam":
-        return Nadam(lr=val)
-
 def genOptDict():
     optDict = { 
         'Adam':[0.0001]
@@ -128,8 +108,8 @@ def loadData(dbPath='data/data.db'):
     data = cursor.execute('''
 
         SELECT redName, blueName, winner, 
-            CAST(red.wins AS FLOAT)/(red.wins+red.losses) AS winrate, red.mu, red.sigma, 
-            CAST(blue.wins AS FLOAT)/(blue.wins+blue.losses) AS winrate, blue.mu, blue.sigma 
+            CAST(red.wins AS FLOAT)/(red.wins+red.losses) AS winrate, strftime('%s', red.avgMatchTime)-strftime('%s', '00:00:00') AS avgTimeRed, red.mu, red.sigma, 
+            CAST(blue.wins AS FLOAT)/(blue.wins+blue.losses) AS winrate, strftime('%s', blue.avgMatchTime)-strftime('%s', '00:00:00') AS avgTimeBlue, blue.mu, blue.sigma 
             from fights 
             INNER JOIN characters AS red ON fights.redName = red.name and fights.mode = red.mode and (fights.mode = "Matchmaking" or fights.mode = "Tournament")
             INNER JOIN characters AS blue ON fights.blueName = blue.name and fights.mode = blue.mode and (fights.mode = "Matchmaking" or fights.mode = "Tournament")
@@ -141,11 +121,12 @@ def loadData(dbPath='data/data.db'):
 
     df = pandas.DataFrame(data)
 
-    for value in [4,5,7,8]:
+    for value in [4,5,6,8,9,10]:
         df[value] = (df[value]-df[value].min()) / (df[value].max()-df[value].min())
 
-    # return data
     return df.values.tolist()
+
+    # return data
 
 def makeData(data, nameDict, samples=10000):
     # mid = random.randint(int(samples/2),len(data)-int(samples/2))
@@ -161,111 +142,3 @@ def makeData(data, nameDict, samples=10000):
     evalData, evalLabels = buildMatrix(dataTemp[int(0.9*len(dataTemp)):], nameDict, isSparse=0)
 
     return trainData, trainLabels, trainEvalData, trainEvalLabels, evalData, evalLabels
-
-def genModel(nameCount, dataCount, optimizer):
-
-    units = int(nameCount/8)
-    # units = nameCount
-
-    nameInput = Input(shape=(nameCount,), name='nameInput')
-    x = Dense(units, kernel_regularizer=l2(l=0.0001))(nameInput)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    nameOutput = Dense(nameCount, name='nameOutput')(x)
-
-    dataInput = Input(shape=(dataCount,), name='dataInput')
-    x = Dense(dataCount, kernel_regularizer=l2(l=0.0001))(dataInput)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    x = Dense(dataCount, kernel_regularizer=l2(l=0.0001))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    # x = Dense(dataCount, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    # x = Dense(dataCount, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    dataOutput = Dense(dataCount, name='dataOutput')(x)
-
-    x = keras.layers.concatenate([nameOutput, dataOutput])
-
-    x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.5)(x)
-
-    x = Dense(nameCount)(x)
-    mainOutput = Activation('softmax', name='mainOutput')(x)
-
-    model = Model(inputs=[nameInput, dataInput], outputs=[mainOutput])
-
-    model.compile(loss='categorical_crossentropy',
-        optimizer = optimizer,
-        metrics=['acc'])
-
-    plot_model(model, to_file='model.png')
-
-    model.summary()
-
-    return model
---
+++ model.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+import keras
+from keras.models import Model
+from keras.layers import Input, Dense, Activation, Dropout, GaussianNoise
+from keras.optimizers import SGD, RMSprop, Adagrad, Adadelta, Adam, Adamax, Nadam
+from keras.regularizers import l2
+from keras.layers.normalization import BatchNormalization
+from keras.utils import plot_model
+
+def optimizer(name, val):
+    if name == "SGD":
+        return SGD(lr=val)
+    if name == "RMSprop":
+        return RMSprop(lr=val)
+    if name == "Adagrad":
+        return Adagrad(lr=val)
+    if name == "Adadelta":
+        return Adadelta(lr=val)
+    if name == "Adam":
+        return Adam(lr=val)
+    if name == "Adamax":
+        return Adamax(lr=val)
+    if name == "Nadam":
+        return Nadam(lr=val)
+
+def genModel(nameCount, dataCount, optimizer):
+
+    units = int(nameCount/8)
+    # units = nameCount
+
+    nameInput = Input(shape=(nameCount,), name='nameInput')
+    x = Dense(units, kernel_regularizer=l2(l=0.0001))(nameInput)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+
+    x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+
+    x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+
+    nameOutput = Dense(nameCount, name='nameOutput')(x)
+
+    dataInput = Input(shape=(dataCount,), name='dataInput')
+    x = Dense(dataCount, kernel_regularizer=l2(l=0.0001))(dataInput)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    # x = Dropout(0.5)(x)
+
+    # x = Dense(dataCount, kernel_regularizer=l2(l=0.0001))(x)
+    # x = Activation('relu')(x)
+    # x = BatchNormalization()(x)
+    # # x = Dropout(0.5)(x)
+
+    dataOutput = Dense(dataCount, name='dataOutput')(x)
+
+    x = keras.layers.concatenate([nameOutput, dataOutput])
+
+    x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    # x = Dropout(0.5)(x)
+
+    # x = Dense(units, kernel_regularizer=l2(l=0.0001))(x)
+    # x = Activation('relu')(x)
+    # x = BatchNormalization()(x)
+    # # x = Dropout(0.5)(x)
+
+    x = Dense(nameCount)(x)
+    x = BatchNormalization()(x)
+    mainOutput = Activation('softmax', name='mainOutput')(x)
+
+    model = Model(inputs=[nameInput, dataInput], outputs=[mainOutput])
+
+    model.compile(loss='categorical_crossentropy',
+        optimizer = optimizer,
+        metrics=['accuracy'])
+
+    plot_model(model, to_file='model.png')
+
+    model.summary()
+
+    return model
